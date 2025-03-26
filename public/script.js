6732ee15febe9fad0a307ba41a94c314adb0b045
rapidroutines <rapidroutines@gmail.com>
2025-03-26 00:49:49 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -26,7 +26,7 @@ async function handleOutgoingChat() {
     };
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/generate", {
+        const response = await fetch("https://render-chatbot-2.onrender.com/generate", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
